2490db28b8f69e8ccb91db2b08fb02d4062055f5
Jonathan05-creator <72807458+Jonathan05-creator@users.noreply.github.com>
2020-11-30 23:47:11 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 while True:
     print("Current Room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) + "°F" + " - " + input.temperature(TemperatureUnit.CELSIUS) + "°C")
     if input.temperature(TemperatureUnit.FAHRENHEIT) >= 195:
-        light.set_all(light.rgb(255,128,0))
+        light.set_all(light.rgb(255,0,0))
     elif input.temperature(TemperatureUnit.FAHRENHEIT) > 100:
         light.set_all(light.rgb(255,0,255))
     else:
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     console.log("Current Room Temperature: " + input.temperature(TemperatureUnit.Fahrenheit) + "°F" + " - " + input.temperature(TemperatureUnit.Celsius) + "°C")
     if (input.temperature(TemperatureUnit.Fahrenheit) >= 195) {
-        light.setAll(light.rgb(255, 128, 0))
+        light.setAll(light.rgb(255, 0, 0))
     } else if (input.temperature(TemperatureUnit.Fahrenheit) > 100) {
         light.setAll(light.rgb(255, 0, 255))
     } else {
