0f18d172abf89a7de7be3eec5f2598696f01b061
Jonathan05-creator <72807458+Jonathan05-creator@users.noreply.github.com>
2020-11-30 23:31:59 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 while (true) {
     console.log("Current Room Temperature: " + input.temperature(TemperatureUnit.Fahrenheit) + "°F" + " - " + input.temperature(TemperatureUnit.Celsius) + "°C")
-    if (input.temperature(TemperatureUnit.Fahrenheit) >= 81) {
+    if (input.temperature(TemperatureUnit.Fahrenheit) >= 195) {
         light.setAll(light.rgb(255, 128, 0))
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) > 50) {
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) > 100) {
         light.setAll(light.rgb(255, 0, 255))
     } else {
         light.setAll(light.rgb(0, 0, 255))
